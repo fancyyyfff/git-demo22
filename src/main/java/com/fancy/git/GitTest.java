340ac3718639944890b33937523d90dfb6836cc3
fancy <f16fy@outlook.com>
2023-07-22 09:13:02 +0800

    push test

--- src/main/java/com/fancy/git/GitTest.java
+++ src/main/java/com/fancy/git/GitTest.java
@@ -1,2 +1,7 @@
-package com.fancy.git;public class GitTest {
+package com.fancy.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git1");
+    }
 }
